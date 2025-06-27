3f0a8372788bddd1d34ffea30c480df18ec6144c
ahoekdev <ahoekdev@gmail.com>
2025-06-27 20:14:00 +0200

    refactor from weather forecast to users

---
+++ api/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController(ILogger<UsersController> logger) : ControllerBase
+{
+    private static readonly User[] Users =
+    [
+        new User { Name = "User 1", Email = "user1@example.com" },
+        new User { Name = "User 2", Email = "user2@example.com" },
+        new User { Name = "User 3", Email = "user3@example.com" },
+        new User { Name = "User 4", Email = "user4@example.com" },
+        new User { Name = "User 5", Email = "user5@example.com" }
+    ];
+
+    [HttpGet(Name = "GetUsers")]
+    public IEnumerable<User> Get()
+    {
+        logger.LogInformation("Fetching users");
+        return Users;
+    }
+}
