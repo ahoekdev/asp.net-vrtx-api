b54bb2f0b293c3ed59df9f22ecf2ede81b111a1a
ahoekdev <ahoekdev@gmail.com>
2025-06-28 10:21:01 +0200

    convert user role from int to string with migrations

--- api/Data/ApplicationDbContext.cs
+++ api/Data/ApplicationDbContext.cs
@@ -1,10 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using api.Entities;
+using api.Enums;
 
 namespace api.Data
 {
   public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
   {
     public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+      modelBuilder.Entity<User>()
+          .Property(u => u.Role)
+          .HasConversion(
+              v => v.ToString().ToUpper(),
+              v => (UserRole)Enum.Parse(typeof(UserRole), v, true));
+    }
   }
 }
