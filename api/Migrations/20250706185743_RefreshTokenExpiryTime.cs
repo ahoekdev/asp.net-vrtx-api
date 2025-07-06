323d09d90a2e05c9ae0c50c677f0feddc6a390f5
ahoekdev <ahoekdev@gmail.com>
2025-07-06 20:58:42 +0200

    add migration for adding refresh token expirty date

---
+++ api/Migrations/20250706185743_RefreshTokenExpiryTime.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RefreshTokenExpiryTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "refresh_token_expiry_time",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "refresh_token_expiry_time",
+                table: "users");
+        }
+    }
+}
